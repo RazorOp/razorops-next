3d34c146e3f06c8566feb916f777794c374511aa
kshyam <shyam@codecrux.com>
2025-12-27 03:55:56 +0530

    iframe fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Enables static exports
+  // Set the base path to your repository name, e.g., "/my-blog-repo"
+  basePath: "/razorops-next",
+  assetPrefix: "/razorops-next/",
+  images: {
+    unoptimized: true, // GitHub Pages does not support Next.js image optimization by default
+  }
 };
 
 export default nextConfig;
